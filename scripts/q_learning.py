64f31e5663e8a884a9ac9673e91341d87fe945c3
Evan Wu <evan.b.wu@gmail.com>
2022-05-01 10:43:58 -0500

    updated q-learning script

--- scripts/q_learning.py
+++ scripts/q_learning.py
@@ -5,13 +5,15 @@ import numpy as np
 import os
 
 # import custom message types from project
-from q_learning.msg import QMatrix, QLearningReward, RobotMoveObjectToTag
+from q_learning_project.msg import QMatrix, QLearningReward, RobotMoveObjectToTag
 
 # Path of directory on where this file is located
 path_prefix = os.path.dirname(__file__) + "/action_states/"
 
 class QLearning(object):
     def __init__(self):
+        self.initialized = False 
+
         # Initialize this node
         rospy.init_node("q_learning")
 
@@ -20,7 +22,9 @@ class QLearning(object):
         self.robot_action_pub = rospy.Publisher("robot_action", RobotMoveObjectToTag, queue_size=10)
         rospy.Subscriber("q_learning/reward", QLearningReward, self.get_reward())
 
-        self.msgReceived = False
+        # Initialize variables to store information from reward subscriber
+        self.reward = 0
+        self.msg_received = False
 
         # Fetch pre-built action matrix. This is a 2d numpy array where row indexes
         # correspond to the starting state and column indexes are the next states.
@@ -29,7 +33,7 @@ class QLearning(object):
         # from the starting state. Values 0-9 correspond to what action is needed
         # to go to the next state.
         #
-        # e.g. self.action_matrix[0][12] = 5
+        # e.g. self.action_matrix[0,12] = 5
         self.action_matrix = np.loadtxt(path_prefix + "action_matrix.txt")
 
         # Fetch actions. These are the only 9 possible actions the system can take.
@@ -43,7 +47,6 @@ class QLearning(object):
             self.actions
         ))
 
-
         # Fetch states. There are 64 states. Each row index corresponds to the
         # state number, and the value is a list of 3 items indicating the positions
         # of the pink, green, blue dumbbells respectively.
@@ -55,80 +58,93 @@ class QLearning(object):
         self.states = list(map(lambda x: list(map(lambda y: int(y), x)), self.states))
 
         # Initialize Q-matrix [64 x 9], rows are states, columns are actions
-        self.Q = np.array((64, 9))
+        self.Q = np.zeros((64, 9))
 
-        # Variables for convergence
-        self.convCount = 0 # count how many iterations since last significant change for convergence check
-        self.convChange = 0.0001 # maximum allowed change for convergence
-        self.convNum = 100 # number of iterations with no change before matrix considered converged
+        self.num_conv = 0 # track number of times matrix is converged
+
+        self.initialized = True
 
 
     def get_reward(self, data):
-        # gets the reward from the environment
+        # called by reward subscriber to get reward after robot performs an action
         self.reward = data.reward
-        self.msgReceived = True
+        self.msg_received = True
 
-    def is_converged(self, prevQ):
-        diffs = np.subtract(prevQ, self.Q)
-        # absDiffs = np.absolute(diffs)
-        maxDiff = max( max(diffs), abs(min(diffs)) ) # accounts for negative difference
-        self.convCount = self.convCount + 1 if maxDiff < convChange else 0
-        return self.convCount >= convNum
+
+    def is_converged(self, prevQ, epsilon, conv):
+        # checks if all elements in the Q matrix changed significantly (> epsilon)
+        # if converged for conv iterations returns true
+        abs_diff = np.abs(self.Q - prevQ)
+        if max(abs_diff) < epsilon:
+            self.num_conv += 1
+        
+        return self.num_conv == conv
 
 
     def q_learning_algorithm(self):
-        t = 0 # time step
+        # main loop for the Q-learning algorithm
+        # iteratively update the Q matrix by performing random actions until converged
+        t = 0 
+        state = 0 # index of current state
+        gamma = 0.8 # discount factor
+
         converged = False # indicator for whether Q has converged
-        state = 0
+        epsilon = 0.001 # threshold for convergence
+        conv = 10 # number of iterations repeated for convergence
 
-        gamma = 0.8
-
-        while converged == False:
+        while not converged:
             # get list of possible next states
-            possible_states = [i for i in len(self.action_matrix[state]) if self.action_matrix[state][i] != -1]
+            possible_states = [i for i in range(self.action_matrix.shape[1]) if self.action_matrix[state,i] != -1]
 
-            # if we reached a final state (all three objects in front of tags), the world resets itself, so we update our current state
-            if (possible_states == []).all(): # using numpy.all() for array comparison
+            # if we reached a final state the world resets
+            if len(possible_states) == 0:
                 state = 0
-                possible_states = [i for i in len(self.action_matrix[state]) if self.action_matrix[state][i] != -1]
+                possible_states = [i for i in range(self.action_matrix.shape[1]) if self.action_matrix[state,i] != -1]
 
-            # continue regardless of whether reset or not
-            next_state = np.random.choice(possible_states, size = 1)
-            action_num = self.action[state][next_state]
+            # sample next state uniformly from possible transitions and get action required
+            next_state = np.random.choice(possible_states)
+            action = self.action_matrix[state, next_state]
+            action = self.actions[action]
 
             # publish chosen action
-            action = RobotMoveObjectToTag()
-            colorID = self.actions[action][1]
-            action.robot_object = "pink" if colorID==0 else "green" if colorID==1 else "blue"
-            robot_object.tag_id = self.actions[action][1]
-            self.action_pub.publish(action)
+            self.robot_action_pub.publish(RobotMoveObjectToTag(action['object'], action['tag']))
 
-            while not self.msgReceived: rospy.spin()
+            # get reward, wait for subscriber to update
+            while not self.msg_received: rospy.spin()
             reward = self.reward # check that we are actually getting a reward
             self.msgReceived = False
 
             # save prev matrix for convergence check
-            prevQ = self.Q.copy() # deep copy with numpy.ndarray.copy()
-            # Update Q_matrix
-            self.Q[state, action] = reward + (gamma * np.max(self.Q[next_state, :]))
+            prevQ = np.copy(self.Q)
 
-            # Set next state
+            # update Q_matrix and publish to topic as flattened row-major array
+            self.Q[state, action] = reward + (gamma * np.max(self.Q[next_state, :]))
+            self.q_matrix_pub(q_matrix = self.Q.flatten())
+
+            # set next state as current state
             state = next_state
 
+            # check for convergence
+            converged = self.is_converged(prevQ, epsilon, conv)
+
             t += 1
-            converged = is_converged(prevQ)
-
-
-    def run(self):
-        self.q_learning_algorithm()
 
 
     def save_q_matrix(self):
-        # TODO: You'll want to save your q_matrix to a file once it is done to
-        # avoid retraining
-        return
+        # save the q_matrix to .npy file
+        file = open("trained_q_matrix", "w")
+        np.save(file, self.Q)
+        file.close
+
+
+    def run(self):
+        # make sure everything's initialized 
+        while not self.initialized: rospy.spin()
+
+        self.q_learning_algorithm()
+        # self.save_q_matrix()
+        
 
 if __name__ == "__main__":
     node = QLearning()
     node.run()
-    rospy.spin()
