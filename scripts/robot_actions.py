98c4c9c1404f8cb8ccc6451cf4cc6177bb5edf32
Evan Wu <evan.b.wu@gmail.com>
2022-05-05 14:56:35 -0500

    updated image processing

--- scripts/robot_actions.py
+++ scripts/robot_actions.py
@@ -4,6 +4,7 @@ import rospy, os, cv2, cv_bridge
 import numpy as np
 
 from sensor_msgs.msg import LaserScan, Image
+from geometry_msgs.msg import Vector3, Twist
 
 # import custom message types from project
 from q_learning_project.msg import RobotMoveObjectToTag
@@ -11,42 +12,19 @@ from q_learning_project.msg import RobotMoveObjectToTag
 # Path of directory on where this file is located
 path_prefix = os.path.dirname(__file__) + "/action_states/"
 
-
-class RobotActions(object):
+# publisher node to choose and publish optimal actions
+class ChooseRobotActions(object):
     def __init__(self):
-        self.initialized = False
-
         # initialize node
-        rospy.init_node("robot_actions")
+        rospy.init_node("choose_robot_actions")
 
-        # set up robot action publisher and subscriber
-        self.robot_action_pub = rospy.Publisher("/q_learning/robot_action", RobotMoveObjectToTag, queue_size=10)       
-        rospy.Subscriber("/q_learning/robot_action", RobotMoveObjectToTag, self.robot_action_received)
-        rospy.Subscriber("/scan", LaserScan, self.scan_received)
-        self.bridge = cv_bridge.CvBridge()
-        rospy.Subscriber("/camera/rgb/image_raw", ) # fill this in
-        # action publisher too 
+        # set up robot action publisher
+        self.robot_action_pub = rospy.Publisher("/q_learning/robot_action", RobotMoveObjectToTag, queue_size=10)
 
-        # set up necessary variables
-        self.color = ""
-        self.tag = 0
-        self.closest_object = # angle or distance or both or ....
-        self.camera_feed = #something camera
-
-        # Fetch pre-built action matrix. This is a 2d numpy array where row indexes
-        # correspond to the starting state and column indexes are the next states.
-        #
-        # A value of -1 indicates that it is not possible to get to the next state
-        # from the starting state. Values 0-9 correspond to what action is needed
-        # to go to the next state.
-        #
-        # e.g. self.action_matrix[0,12] = 5
+        # Fetch pre-built action matrix
         self.action_matrix = np.loadtxt(path_prefix + "action_matrix.txt")
 
-        # Fetch actions. These are the only 9 possible actions the system can take.
-        # self.actions is an array of dictionaries where the row index corresponds
-        # to the action number, and the value has the following form:
-        # { object: "pink", tag: 1}
+        # Fetch actions
         colors = ["pink", "green", "blue"]
         self.actions = np.loadtxt(path_prefix + "actions.txt")
         self.actions = list(map(
@@ -54,75 +32,114 @@ class RobotActions(object):
             self.actions
         ))
 
-        # Fetch states. There are 64 states. Each row index corresponds to the
-        # state number, and the value is a list of 3 items indicating the positions
-        # of the pink, green, blue dumbbells respectively.
-        # e.g. [[0, 0, 0], [1, 0 , 0], [2, 0, 0], ..., [3, 3, 3]]
-        # e.g. [0, 1, 2] indicates that the green dumbbell is at block 1, and blue at block 2.
-        # A value of 0 corresponds to the origin. 1/2/3 corresponds to the block number.
-        # Note: that not all states are possible to get to.
-        self.states = np.loadtxt(path_prefix + "states.txt")
-        self.states = list(map(lambda x: list(map(lambda y: int(y), x)), self.states))
-
         # read in converged Q-matrix
         self.Q = np.loadtxt(path_prefix + "trained_q_matrix.csv", delimiter = " ")
 
-        self.initialized = True 
 
-    
-    def robot_get_actions(self):
+    def choose_robot_actions(self):
         # get optimal actions from converged Q-matrix and publish
         state = 0 
 
-        for i in range(3):
+        while np.where(self.Q[state,:] == 0).shape[0] != 0:
             # choose action with highest Q-value, or randomly if multiple are the max
             possible_states = self.Q[state,:]
             optimal_action_ind = np.random.choice(np.argwhere(possible_states == np.amax(possible_states)))
 
-            # get action and publish (IDK if best approach, as we want actions to happen sequentially)
+            # get action and publish 
             action = self.action_matrix[optimal_action_ind]
             self.robot_action_pub.publish(RobotMoveObjectToTag(action['object'], action['tag']))
-            # maybe wait for some sort of confirmation of action finished from subscriber
 
             # update current state
             state = np.argwhere(self.action_matrix[state,:] == optimal_action_ind)
 
 
+
+# subscriber node to execute published actions
+class RobotMovement(object):
+    def __init__(self):
+        # initialize node
+        rospy.init_node("robot_movement")
+
+        # set up subscribers to robot actions and sensors      
+        rospy.Subscriber("/q_learning/robot_action", RobotMoveObjectToTag, self.robot_action_received)
+        rospy.Subscriber("/scan", LaserScan, self.scan_received)
+
+        # set up publisher to move robot
+        self.robot_movement_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+
+        # set up camera settings 
+        self.bridge = cv_bridge.CvBridge()
+        rospy.Subscriber("/camera/rgb/image_raw", Image, self.image_received)
+
+        # get aruco dictionary
+        self.aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+
+        # queue to store actions to perform
+        self.action_queue = []
+        self.finished_actions = 0 # number of actions performed
+
+        # set up necessary variables
+        self.min_dist = 0 
+        self.curr_image = []# something camera
+
+
+    def execute_actions(self):
+        # constant loop to perform robot actions
+        while True:
+            # wait if no actions to perform
+            while len(self.action_queue) == 0:
+                if self.finished_actions < 3:
+                    rospy.sleep(1)
+                else:
+                    return
+
+            # call necessary functions to perform the action
+            self.move_object()
+            self.move_to_origin()
+            self.move_to_tag()
+            self.move_to_origin()
+
+            # pop the action
+            self.action_queue.pop(0)
+            self.finished_actions += 1
+
+
     def robot_action_received(self, data):
-        # subscriber function 
-        self.color = data['object'] #'pink'
-        self.tag = data['tag'] # 1
-
-        # detect object
-        move_to_tube(color = color)
-        move_to_tag(tag = tag)
-
-        # set finished action maybe move back to origin
+        # append robot actions to queue
+        self.action_queue.append(data)
 
 
     def scan_received(self, data):
-        # get measurements from closest object so that we can move to each thing 
+        # get distance from closest object (I don't think we need angles)
+        ranges = np.nan_to_num(data.ranges)
+        self.min_dist = np.amin(ranges)
 
 
-    def picture_received(self, data):
-        # get measurements from robot camera to detect 
+    def image_received(self, data):
+        # get current image from robot camera 
+        self.
 
 
-    def move_to_tube(self, color):
+    def move_object(self, color):
         # detect tube, spin around if not in camera frame
         while not detecting_color: 
             # spin around
             publish.(angular velocity = 0.1)
 
         # once the tube is in the frame orient it to the center
-        # reuse line follower code
+        # reuse line follower code, stop dist = 0.237
 
         # move to correct distance 
 
-        # pick up the tube
+        # pick up the tube, first open gripper to max, then descend and pick it up 
+
+
+    def move_to_origin(self):
+    # move back to origin 
 
     
     def move_to_tag(self, tag): 
+        corners, ids, rejected_points = cv2.aruco.detectMarkers(grayscale_image, self.aruco_dict)
         # move to the tag
         while not detecting_tag:
             #spin around
@@ -132,3 +149,14 @@ class RobotActions(object):
         # move to correct distance
 
         # drop tube
+
+
+
+if __name__ == "main":
+    pub = ChooseRobotActions()
+    sub = RobotMovement()
+    rospy.sleep(3)
+    pub.choose_robot_actions()
+    sub.execute_actions()
+    rospy.spin()
+
