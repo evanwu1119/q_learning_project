c872cf4d9fd603a46f1520ebdc4b0bbf158fd151
Zoa <zoa.katok@gmail.com>
2022-05-09 20:37:38 +0000

    refactor

--- scripts/arm-move-test.py
+++ scripts/arm-move-test.py
@@ -9,46 +9,45 @@ class Robot(object):
     def __init__(self):
         rospy.init_node("tb3")
 
-        # self.move_group_arm = moveit_commander.MoveGroupCommander("arm")
-        # self.move_group_gripper = moveit_commander.MoveGroupCommander("gripper")
+        self.move_group_arm = moveit_commander.MoveGroupCommander("arm")
+        self.move_group_gripper = moveit_commander.MoveGroupCommander("gripper")
 
-        rospy.Subscriber("/scan", LaserScan, self.process_scan)
+    #     rospy.Subscriber("/scan", LaserScan, self.process_scan)
 
-    def process_scan(self, data):
-        print(data.ranges[0])
+    # def process_scan(self, data):
+    #     print(data.ranges[0])
 
     def run(self):
-        gripper_open = [0.008, 0.008]
-        self.move_group_gripper.go(gripper_open, wait=True)
-        rospy.sleep(2)
-
+        gripper_open = [0.009, 0.009]
+        gripper_close = [-0.005, -0.005]
         arm_up = [
             0.0,
             0.0,
             math.radians(10.0),
             0.0           
         ]
-        self.move_group_arm.go(arm_up, wait=True)
-        rospy.sleep(2)
-
         arm_down = [
             0.0,
             math.radians(35.0),
             math.radians(10.0),
             math.radians(-50.0)
         ]
-        self.move_group_arm.go(arm_down, wait=True)
+
+        print("GRIPPER OPEN")
+        self.move_group_gripper.go(gripper_open)
+        rospy.sleep(2)
+        print("ARM DOWN")
+        self.move_group_arm.go(arm_down)
+        rospy.sleep(2)
+        print("GRIPPER CLOSE")
+        self.move_group_gripper.go(gripper_close)
+        rospy.sleep(2)
+        print("ARM UP")
+        self.move_group_arm.go(arm_up)
         rospy.sleep(2)
 
-        gripper_close = [-0.005, -0.005]
-        self.move_group_gripper.go(gripper_close, wait=True)
-        rospy.sleep(2)
-
-        self.move_group_arm.go(arm_up, wait=True)
-        rospy.sleep(2)
         self.move_group_arm.stop()
 
 if __name__ == '__main__':
     node = Robot()
-    rospy.spin()
-    # node.run()
+    node.run()
